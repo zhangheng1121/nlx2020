a689769edd401782a399b983a92bc9c521ba13f4
Zhangheng1121 <1106226910@qq.com>
2020-11-13 16:00:40 +0800

    修改父类的pom文件

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Project_Name: nlx2020
+ * @Introduce:
+ * @Author: zhangheng
+ * @Date: 2020/11/13 11:53
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+}
